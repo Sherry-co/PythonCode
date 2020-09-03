60a94345af65f035cf02aa1daca388378b08eef9
Sherry-co <lichen1025@foxmail.com>
2020-09-03 15:56:03 +0800

    chapter9

--- chapter9/electric_car.py
+++ chapter9/electric_car.py
@@ -67,7 +67,7 @@ class ElectricCar(Car):
         self.battery = Battery()
 
 
-my_tesla = ElectricCar('tesla', 'model s', 2016)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+# my_tesla = ElectricCar('tesla', 'model s', 2016)
+# print(my_tesla.get_descriptive_name())
+# my_tesla.battery.describe_battery()
+# my_tesla.battery.get_range()
--- chapter9/my_car.py
+++ chapter9/my_car.py
@@ -1,6 +1,7 @@
+# 导入类
 from car import Car
 
-"""让Python打开模块car，并导入其中的Car类"""
+"""让Python打开模块car，并导入其中的Car类并创建其实例"""
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 
